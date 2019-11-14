0e36c68ccd004c70e98a6a5e4d137d10102b3ea6
Claus Wolf <cwolf@otzberg.net>
2019-11-14 19:23:17 +0100

    Initial Commit

---
+++ wbm/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  wbm
+//
+//  Created by Claus Wolf on 14.11.19.
+//  Copyright © 2019 Claus Wolf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ wbm/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  wbm
+//
+//  Created by Claus Wolf on 14.11.19.
+//  Copyright © 2019 Claus Wolf. All rights reserved.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = "wbm";
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "net.otzberg.wbm-Extension") { error in
+            if let _ = error {
+                // Insert code to inform the user that something went wrong.
+
+            }
+        }
+    }
+
+}
