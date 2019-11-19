f4cdfe259187926a3c0b1cd28950d1ab376fe2d9
Claus Wolf <cwolf@otzberg.net>
2019-11-19 19:41:26 +0100

    the previous solution was bad
    this solution is better and the regex actually works. Previous solution also matched "gold" as valid URL, likely since it was valid, once I put https:// infront of it

--- wbm Extension/SafariExtensionViewController.swift
+++ wbm Extension/SafariExtensionViewController.swift
@@ -215,11 +215,10 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
     
     @IBAction func wbmURLAction(_ sender: NSTextField) {
         let url = sender.stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
-        let url1 = "https://\(url)"
-        if (url.isValidURL){
+        if (validateUrl(urlString: url)){
             self.openTabWithURL(url: "https://web.archive.org/web/*/\(url)")
         }
-        else if(url1.isValidURL){
+        else if(validateUrl(urlString: "http://\(url)")){
             self.openTabWithURL(url: "https://web.archive.org/web/*/\(url)")
         }
         else{
@@ -227,19 +226,10 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
         }
     }
  
+    func validateUrl (urlString: String?) -> Bool {
+        let urlRegEx = "https?://(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
+        return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: urlString)
+    }
     
 }
 
-
-// Source for String Extension: https://stackoverflow.com/questions/28079123/how-to-check-validity-of-url-in-swift/49072718
-extension String {
-    var isValidURL: Bool {
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
-            // it is a link, if the match covers the whole string
-            return match.range.length == self.utf16.count
-        } else {
-            return false
-        }
-    }
-}
